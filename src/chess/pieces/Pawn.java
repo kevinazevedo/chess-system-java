64303df577edc43405dcd6b8850de4c5759d28f1
Kevin Azevedo <taylorvision95@outlook.com>
2024-01-21 14:29:29 -0300

    Bishop

--- src/chess/pieces/Pawn.java
+++ src/chess/pieces/Pawn.java
@@ -21,8 +21,7 @@ public class Pawn extends ChessPiece {
 		boolean[][] mat = new boolean[getBoard().getRows()][getBoard().getColumns()];
 				
 		Position p = new Position(0, 0);
-		
-		
+			
 		if (getColor() == Color.WHITE) {
 			p.setValues(position.getRow() - 1, position.getColumn());
 			if (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) {
